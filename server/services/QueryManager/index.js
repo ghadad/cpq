f55d1e179cdd93e6fcb7ab795520b5bf2b1de3ea
Golan Hadad <ghadad@gmail.com>
2022-05-28 20:35:18 +0300

    wip

--- server/services/QueryManager/index.js
+++ server/services/QueryManager/index.js
@@ -1,13 +1,24 @@
 'use strict'
+const find = require("find")
+const fs = require("fs")
+const JsYaml = require("js-yaml");
+const path = require("path");
 
  class QueryManager { 
-   name="QueryManager"
+  name="QueryManager"
+  inventoryJson = {} ;
+  files = []
   constructor() {
    
   }
-  
+
   load() {
-    return "loaded";
+       this.files = find.fileSync(/\.(yaml|yml)$/, process.env.CQP_INVENTORY);//.map(e=> e.replace(process.env.CQP_INVENTORY,'').replace(/^[\\\/]/g,""));
+        this.files.forEach(e => {
+          this.inventoryJson[e] = JsYaml.load(fs.readFileSync(e,'utf8'))
+       });
+       return this.inventoryJson;
+
   }
 }
 
